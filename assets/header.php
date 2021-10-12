40f9d05162a0bff194ffb8db75214d1a33fd86e4
Thomaz <79727201+thomazgg@users.noreply.github.com>
2021-10-12 12:09:21 -0300

    Add files via upload

---
+++ assets/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="sortcut icon" href="../assets/img/form.png" />
+
+    <title>Formulario com BD</title>
+    
+    <!-- Styles -->
+	<link rel="stylesheet" href="../assets/style_all.css">
+</head>
